2c0ed6084b52646c75aeb052c4909f76803ea354
MattGueler <mateus.gueler@gmail.com>
2022-10-29 15:38:34 -0300

    refactor:
    Update function to create a room reserve in database

--- src/controllers/hotel-controller.ts
+++ src/controllers/hotel-controller.ts
@@ -1,21 +1,23 @@
-import { Request, Response } from "express";
+import { AuthenticatedRequest } from '@/middlewares';
+import { Request, Response } from 'express';
 
-import * as hotelServices from "../services/hotel-service/index";
+import * as hotelServices from '../services/hotel-service/index';
 
 export async function getAllByEventId(req: Request, res: Response) {
-    const { session } = res.locals
-    const { eventId } = req.params
-    
-    const hotels = await hotelServices.getAllByEventId(+eventId, session)
+  const { session } = res.locals;
+  const { eventId } = req.params;
 
-    res.status(200).send(hotels)
+  const hotels = await hotelServices.getAllByEventId(+eventId, session);
+
+  res.status(200).send(hotels);
 }
 
-export async function createOrUpdateReservation(req: Request, res: Response) {
-    const {orderId, roomId} = req.body;
-    const { eventId } = req.params
+export async function createOrUpdateReservation(req: AuthenticatedRequest, res: Response) {
+  const { roomId } = req.body;
+  const { eventId } = req.params;
+  const { userId } = req;
 
-    await hotelServices.createOrUpdateReservation(orderId, roomId, eventId);
+  await hotelServices.createOrUpdateReservation(roomId, Number(eventId), userId);
 
-    res.status(200).send("Reservation completed.");
+  res.status(200).send('Reservation completed.');
 }

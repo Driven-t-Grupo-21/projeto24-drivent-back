8c55205d550ac6b799de8359bbc75990b207dd09
MattGueler <mateus.gueler@gmail.com>
2022-11-01 17:22:20 -0300

    :dart: chore:
    Get infos about activity by date

--- src/repositories/activities-repository/index.ts
+++ src/repositories/activities-repository/index.ts
@@ -1,12 +1,27 @@
 import { prisma } from '@/config';
 
-async function findActivities() {
+async function findActivitiesDate() {
   return prisma.activities.groupBy({
     by: ['activityDate'],
   });
 }
 
+async function findActivities(date: any) {
+  return prisma.activities.findMany({
+    where: {
+      activityDate: date,
+    },
+    select: {
+      id: true,
+      local: true,
+      vacancies: true,
+      Activity: true
+    }
+  });
+}
+
 const activitiesRepository = {
+  findActivitiesDate,
   findActivities,
 };
 
--- src/services/activities-service/index.ts
+++ src/services/activities-service/index.ts
@@ -3,7 +3,14 @@ import activitiesRepository from '@/repositories/activities-repository';
 
 // async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
 async function getAllActivities() {
-  const allActivities = await activitiesRepository.findActivities();
+  const allDates = await activitiesRepository.findActivitiesDate();
+
+  const allActivities = await Promise.all(
+    allDates.map(async (date) => {
+      const activies = await activitiesRepository.findActivities(date.activityDate);
+      return { date, activies };
+    }),
+  );
   return allActivities;
 }
 

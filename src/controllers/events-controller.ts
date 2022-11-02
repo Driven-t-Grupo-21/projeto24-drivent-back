b63ef47215a7eed562cba6fa8f7578bd86b451fc
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-11-02 03:16:48 -0300

    Redis DB layer implemented for events

--- src/controllers/events-controller.ts
+++ src/controllers/events-controller.ts
@@ -1,9 +1,28 @@
 import eventsService from '@/services/events-service';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { createClient } from 'redis';
+
+const redis = createClient();
+
+redis.connect();
 
 export async function getDefaultEvent(_req: Request, res: Response) {
-  const event = await eventsService.getFirstEvent();
+  const cacheKey = 'events';
+  const cacheExpiration = 10000;
 
-  return res.status(httpStatus.OK).send(event);
+  try {
+    const cachedEvents = await redis.get(cacheKey);
+    if (cachedEvents) {
+      res.status(httpStatus.OK).send(JSON.parse(cachedEvents));
+    } else {
+      const event = await eventsService.getFirstEvent();
+
+      redis.setEx(cacheKey, cacheExpiration, JSON.stringify(event));
+
+      return res.status(httpStatus.OK).send(event);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }

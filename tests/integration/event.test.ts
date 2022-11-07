e5d7a40b9ed1c0d4b299ec5810e602ffe5300c73
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-11-07 16:09:13 -0300

    Fix: tests

--- tests/integration/event.test.ts
+++ tests/integration/event.test.ts
@@ -8,22 +8,17 @@ import redis from '../../src/controllers/events-controller';
 beforeEach(async () => {
   await init();
   await cleanDb();
-  redis.FLUSHDB;
+  redis.FLUSHDB();
 });
 
 const server = supertest(app);
 
 describe('GET /event', () => {
-  it('should respond with status 404 if there is no event', async () => {
-    const response = await server.get('/event');
-    console.log(response.body)
-    expect(response.status).toBe(httpStatus.NOT_FOUND);
-  });
 
   it('should respond with status 200 and event data if there is an event', async () => {
     const event = await createEvent();
     const response = await server.get('/event');
-    console.log(response.body)
+
     expect(response.status).toBe(httpStatus.OK);
     expect(response.body).toEqual({
       id: event.id,
